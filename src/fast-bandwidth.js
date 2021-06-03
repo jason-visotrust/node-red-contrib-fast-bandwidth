1346299c02d474e32164c254f3e423ea0fb91441
@jcarreira1 <jcarreira@gmail.com>
2021-06-03 07:47:01 -0400

    0.0.3 release: Upgraded pupeteer and pulled in some changes from the fast-cli project

--- src/fast-bandwidth.js
+++ src/fast-bandwidth.js
@@ -1,26 +1,32 @@
 const api = require('./api')
 
-module.exports = function(RED) {
-    function TestBandwidth(config) {
-        RED.nodes.createNode(this,config);
-        const node = this;
+module.exports = function (RED) {
+    function TestBandwidth (config) {
+        RED.nodes.createNode(this, config)
+        const node = this
 
-        node.on('input', async function(msg) {
-            let data = {};
-            node.status({fill: "blue", shape: "dot", text: `fetching...`});
+        node.on('input', async function (msg) {
+            let data = {}
+            node.status({ fill: 'blue', shape: 'dot', text: 'fetching...' })
             try {
-                await api({measureUpload: config.upload}).forEach(result => {
-                    data = result;
-                    node.status({fill: "blue", shape: "dot", text: `fetching... download: ${data.downloadSpeed} ${data.downloadUnit}`});
-                });
-                msg.payload = data;
-                node.status({fill: "green", shape: "dot", text: `done! download: ${data.downloadSpeed} ${data.downloadUnit}`});
+                await api({ measureUpload: config.upload }).forEach(result => {
+                    data = result
+                    node.status(
+                        {   fill: 'blue',
+                            shape: 'dot',
+                            text: `fetching... download: ${data.downloadSpeed} ${data.downloadUnit}` })
+                })
+                msg.payload = data
+                node.status(
+                    {   fill: 'green',
+                        shape: 'dot',
+                        text: `done! download: ${data.downloadSpeed} ${data.downloadUnit}` })
             } catch (e) {
-                console.error(e);
-                node.status({fill: "red", shape: "dot", text: `error: ${e.message}`})
+                console.error(e)
+                node.status({ fill: 'red', shape: 'dot', text: `error: ${e.message}` })
             }
-            node.send(msg);
-        });
+            node.send(msg)
+        })
     }
-    RED.nodes.registerType("fast-bandwidth",TestBandwidth);
+    RED.nodes.registerType('fast-bandwidth', TestBandwidth)
 }
--- test/fast-bandwidth.spec.js
+++ test/fast-bandwidth.spec.js
@@ -1,34 +1,35 @@
 const { describe, it } = require('mocha')
-const helper = require("node-red-node-test-helper");
-const fastNode = require("../src/fast-bandwidth.js");
+const helper = require('node-red-node-test-helper')
+const fastNode = require('../src/fast-bandwidth.js')
 
 describe('fast-bandwidth Node', function () {
 
+    // eslint-disable-next-line no-undef
     afterEach(function () {
-        helper.unload();
-    });
+        helper.unload()
+    })
 
     it('should be loaded', function (done) {
-        var flow = [{ id: "n1", type: "fast-bandwidth", name: "test name" }];
+        const flow = [{ id: 'n1', type: 'fast-bandwidth', name: 'test name' }]
         helper.load(fastNode, flow, function () {
-            var n1 = helper.getNode("n1");
-            n1.should.have.property('name', 'test name');
-            done();
-        });
-    });
+            const n1 = helper.getNode('n1')
+            n1.should.have.property('name', 'test name')
+            done()
+        })
+    })
 
     it('should get bandwidth measurement in payload', function (done) {
-        var flow = [{ id: "n1", type: "fast-bandwidth", name: "test name", measureUpload: false, wires:[["n2"]] },
-            { id: "n2", type: "helper" }];
+        const flow = [{ id: 'n1', type: 'fast-bandwidth', name: 'test name', measureUpload: false, wires: [['n2']] },
+            { id: 'n2', type: 'helper' }]
         helper.load(fastNode, flow, function () {
-            var n2 = helper.getNode("n2");
-            var n1 = helper.getNode("n1");
-            n2.on("input", function (msg) {
-                msg.should.have.property('payload');
-                console.log(msg);
-                done();
-            });
-            n1.receive({ payload: "Ping" });
-        });
-    });
-});
+            const n2 = helper.getNode('n2')
+            const n1 = helper.getNode('n1')
+            n2.on('input', function (msg) {
+                msg.should.have.property('payload')
+                console.log(msg)
+                done()
+            })
+            n1.receive({ payload: 'Ping' })
+        })
+    })
+})
